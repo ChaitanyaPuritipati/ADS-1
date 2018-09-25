ecbe98b000f4425da895d2322036e13fee286c22
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-25 20:55:05 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- ADS-1 Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1 Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int inputsize = Integer.parseInt(scan.nextLine());
-		Percolation pt = new Percolation(inputsize);
-		while(scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			pt.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
-		}
-		System.out.println(pt.percolates());
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int inputsize = Integer.parseInt(scan.nextLine());
+        Percolation pt = new Percolation(inputsize);
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            pt.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+        }
+        System.out.println(pt.percolates());
+    }
 }
