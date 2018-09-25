420883dda453fd2132c664487f6ee9fdd90f832b
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-25 20:33:15 +0530

    "check_style_score -> 7 of 24 passed. style score: 0/1 "

--- ADS-1 Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1 Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -5,7 +5,9 @@ public class Solution {
 		int inputsize = Integer.parseInt(scan.nextLine());
 		Percolation pt = new Percolation(inputsize);
 		while(scan.hasNext()) {
-			pt.open(Integer.parseInt(scan.nextLine()), Integer.parseInt(scan.nextLine()));
+			String line = scan.nextLine();
+			String[] tokens = line.split(" ");
+			pt.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 		}
 		System.out.println(pt.percolates());
 	}
