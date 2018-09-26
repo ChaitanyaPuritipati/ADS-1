a048325f6c6b9432e6bf769db3aec7459126f6a1
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 22:31:55 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1 Assignments/m3/Sorted_Array/Solution.java
@@ -9,22 +9,41 @@ class Solution {
 		int arr1size = Integer.parseInt(scan.nextLine());
 		int arr2size = Integer.parseInt(scan.nextLine());
 		String line = scan.nextLine();
-		System.out.println(line);
+		int[] array1 = new int[arr1size];
+		int[] array2 = new int[arr2size];
 		if (!line.equals("")) {
 			String[] arr1 = line.split(",");
-			int[] array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
+			array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array1);
 			System.out.println(Arrays.toString(array1));
 
 		}
 		line = scan.nextLine();
-		System.out.println(line);
 		if (!line.equals("")) {
 			String[] arr2 = line.split(",");
-			int[] array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
+			array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array2);
 			System.out.println(Arrays.toString(array2));
 		}
-
+		int count = 0;
+		int[] array3 = new int[arr1size + arr2size];
+		for(int i = 0, j = 0; count < arr1size + arr2size; count++) {
+			if(array1[i] < array2[j]) {
+                 array3[count] = array1[i];
+                 i++;
+			}
+			else if(array1[i] > array2[j]) {
+				array3[count] = array2[j];
+				i++;
+				j++;                    	
+			}
+			else if(array1[i] == array2[j]) {
+				array3[count] = array1[i];
+				count++;
+				array3[count] = array2[j];
+				i++;
+			}
+		}
+		System.out.println(Arrays.toString(array3));
 	}
 }
