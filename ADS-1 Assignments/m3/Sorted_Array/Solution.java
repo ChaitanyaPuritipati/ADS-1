371a07215025aeb4aedf03eeb68a92e6d88deee8
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 23:22:49 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1 Assignments/m3/Sorted_Array/Solution.java
@@ -15,21 +15,16 @@ class Solution {
 			String[] arr1 = line.split(",");
 			array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array1);
-			System.out.println(Arrays.toString(array1));
-
 		}
 		line = scan.nextLine();
 		if (!line.equals("")) {
 			String[] arr2 = line.split(",");
 			array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array2);
-			System.out.println(Arrays.toString(array2));
 		}
 		int count = 0;
 		int[] array3 = new int[arr1size + arr2size];
 		for (int i = 0, j = 0; count < arr1size + arr2size; count++) {
-			// System.out.println(i + "i val");
-			// System.out.println(j + "j val");
 			if (i < arr1size) {
 				if (array1[i] < array2[j]) {
 					array3[count] = array1[i];
