116f7497dd6e96f6a6a58bc7be63b363274a4aa5
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 22:06:54 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1 Assignments/m3/Sorted_Array/Solution.java
@@ -6,15 +6,19 @@ class Solution {
 	}
 	public static void main(String[] args) {
 		Scanner scan =  new Scanner(System.in);
-		while (scan.hasNext()) {
-			int arr1size = Integer.parseInt(scan.nextLine());
-			int arr2size = Integer.parseInt(scan.nextLine());
-			String line = scan.nextLine();
+		int arr1size = Integer.parseInt(scan.nextLine());
+		int arr2size = Integer.parseInt(scan.nextLine());
+		String line = scan.nextLine();
+		if (line != null) {
 			String[] arr1 = line.split(",");
 			int[] array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array1);
 			System.out.println(Arrays.toString(array1));
-			line = scan.nextLine();
+
+		}
+		line = scan.nextLine();
+		System.out.println(line);
+		if (line != null) {
 			String[] arr2 = line.split(",");
 			int[] array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
 			Arrays.sort(array2);
