412e7691aa73c4d900824a2c4606a20a88f77915
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 22:59:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/3-sum/Solution.java
+++ ADS-1 Assignments/m3/3-sum/Solution.java
@@ -1,37 +1,51 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
-	Solution() {
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size = Integer.parseInt(scan.nextLine());
-		int[] array = new int[size];
-		for (int i = 0; i < size; i++) {
-			array[i] = Integer.parseInt(scan.nextLine());
-		}
-		int count = 0;
-		Arrays.sort(array);
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size; j++) {
-				int sum = array[i] + array[j];
-				int check = binarySearch(array, j + 1, size - 1, (0 - sum));
-				if (check != -1) {
-					count++;
-				}
-			}
-		}
-		System.out.println(count);
-	}
-	public static int binarySearch(int arr[], int l, int r, int x) {
-		if (r >= l) {
-			int mid = l + (r - l) / 2;
-			if (arr[mid] == x)
-				return mid;
-			if (arr[mid] > x)
-				return binarySearch(arr, l, mid - 1, x);
-			return binarySearch(arr, mid + 1, r, x);
-		}
-		return -1;
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+    }
+    /**
+     * 
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = Integer.parseInt(scan.nextLine());
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = Integer.parseInt(scan.nextLine());
+        }
+        int count = 0;
+        Arrays.sort(array);
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int sum = array[i] + array[j];
+                int check = binarySearch(array, j + 1, size - 1, (0 - sum));
+                if (check != -1) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    /**
+     * { item_description }.
+     */
+    public static int binarySearch(final int arr[], int start, int end, int testvalue) {
+        if (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == testvalue)
+                return mid;
+            if (arr[mid] > testvalue)
+                return binarySearch(arr, start, mid - 1, testvalue);
+            return binarySearch(arr, mid + 1, end, testvalue);
+        }
+        return -1;
+    }
 }
