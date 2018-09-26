aa4adee9738c5daf85adc6a252017a8902ffaeb8
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 23:21:02 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1 Assignments/m3/Sorted_Array/Solution.java
@@ -28,8 +28,8 @@ class Solution {
 		int count = 0;
 		int[] array3 = new int[arr1size + arr2size];
 		for (int i = 0, j = 0; count < arr1size + arr2size; count++) {
-			System.out.println(i + "i val");
-			System.out.println(j + "j val");
+			// System.out.println(i + "i val");
+			// System.out.println(j + "j val");
 			if (i < arr1size) {
 				if (array1[i] < array2[j]) {
 					array3[count] = array1[i];
@@ -43,5 +43,6 @@ class Solution {
 				j++;
 			}
 		}
+		System.out.println(Arrays.toString(array3));
 	}
 }
