32e9cf83b8ed7189c9342174cca4070696596f92
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-26 23:28:52 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1 Assignments/m3/Sorted_Array/Solution.java
+++ ADS-1 Assignments/m3/Sorted_Array/Solution.java
@@ -1,46 +1,56 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
-	Solution() {
-
-	}
-	public static void main(String[] args) {
-		Scanner scan =  new Scanner(System.in);
-		int arr1size = Integer.parseInt(scan.nextLine());
-		int arr2size = Integer.parseInt(scan.nextLine());
-		String line = scan.nextLine();
-		int[] array1 = new int[arr1size];
-		int[] array2 = new int[arr2size];
-		if (!line.equals("")) {
-			String[] arr1 = line.split(",");
-			array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
-			Arrays.sort(array1);
-		}
-		line = scan.nextLine();
-		if (!line.equals("")) {
-			String[] arr2 = line.split(",");
-			array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
-			Arrays.sort(array2);
-		}
-		int count = 0;
-		int[] array3 = new int[arr1size + arr2size];
-		for (int i = 0, j = 0; count < arr1size + arr2size; count++) {
-			if (i < arr1size) {
-				if (array1[i] < array2[j]) {
-					array3[count] = array1[i];
-					i++;
-				} else if (array1[i] > array2[j] && i < arr1size) {
-					array3[count] = array2[j];
-					j++;
-				}
-			} else if (i == arr1size && j < arr2size) {
-				array3[count] = array2[j];
-				j++;
-			}
-		}
-		for(int k = 0; k < array3.length - 1; k++) {
-			System.out.print(array3[k] + ",");
-		}
-		System.out.print(array3[array3.length - 1]);
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        int arr1size = Integer.parseInt(scan.nextLine());
+        int arr2size = Integer.parseInt(scan.nextLine());
+        String line = scan.nextLine();
+        int[] array1 = new int[arr1size];
+        int[] array2 = new int[arr2size];
+        if (!line.equals("")) {
+            String[] arr1 = line.split(",");
+            array1 = Arrays.stream(arr1).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(array1);
+        }
+        line = scan.nextLine();
+        if (!line.equals("")) {
+            String[] arr2 = line.split(",");
+            array2 = Arrays.stream(arr2).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(array2);
+        }
+        int count = 0;
+        int[] array3 = new int[arr1size + arr2size];
+        for (int i = 0, j = 0; count < arr1size + arr2size; count++) {
+            if (i < arr1size) {
+                if (array1[i] < array2[j]) {
+                    array3[count] = array1[i];
+                    i++;
+                } else if (array1[i] > array2[j] && i < arr1size) {
+                    array3[count] = array2[j];
+                    j++;
+                }
+            } else if (i == arr1size && j < arr2size) {
+                array3[count] = array2[j];
+                j++;
+            }
+        }
+        for(int k = 0; k < array3.length - 1; k++) {
+            System.out.print(array3[k] + ",");
+        }
+        System.out.print(array3[array3.length - 1]);
+    }
 }
