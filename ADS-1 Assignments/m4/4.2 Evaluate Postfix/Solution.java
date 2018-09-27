18c73f77486ef30164588e01fb8a42a1e2c4617f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-27 15:17:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 Assignments/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1 Assignments/m4/4.2 Evaluate Postfix/Solution.java
@@ -7,14 +7,14 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String line = scan.nextLine();
-		String[] tokens = line.split("");
+		String[] tokens = line.split(" ");
 		LinkedList<Integer> operands = new LinkedList<Integer>();
 		for (int i = 0; i < tokens.length; i++) {
-			System.out.println(tokens[i]);
+			// System.out.println(tokens[i]);
 			if (!"*/-+".contains(tokens[i])) {
 				operands.add(Integer.parseInt(tokens[i]));
 			} else {
-				System.out.println(tokens[i]);
+				// System.out.println(tokens[i]);
 				switch (tokens[i]) {
 				case "+":
 					int a = operands.removeLast();
