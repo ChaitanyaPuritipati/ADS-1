d1e84f5c895fadd13e159a81c293c56b841af9c3
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-27 15:41:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1 Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -5,79 +5,76 @@ import java.util.Arrays;
  */
 class Solution {
 
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int testnum = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < testnum; i++) {
-			String[] paranthesis = new String[20];
-			String line = scan.nextLine();
-			String[] tokens = line.split("");
-			int size = 0;
-			int flag = 0;
-			for (int j = 0; j < tokens.length; j++) {
-				if (tokens[j].equals("(") || tokens[j].equals("{") || tokens[j].equals("[")) {
-					if (size == paranthesis.length) {
-						paranthesis = Arrays.copyOf(paranthesis, 2 * size);
-					}
-					paranthesis[size] = tokens[j];
-					size++;
-				} else if (tokens[j].equals("}")) {
-					if (size == 0) {
-						flag = 1;
-						System.out.println("NO");
-						break;
-					}
-					if (!paranthesis[size - 1].equals("{")) {
-						System.out.println("NO");
-						flag = 1;
-						break;
-					}
-					size--;
-				} else if (tokens[j].equals(")")) {
-					if (size == 0) {
-						flag = 1;
-						System.out.println("NO");
-						break;
-					}
-					// System.out.println(size + "size value");
-					if (!paranthesis[size - 1].equals("(")) {
-						System.out.println("NO");
-						flag = 1;
-						break;
-					}
-					size--;
-				} else if (tokens[j].equals("]")) {
-					if (size == 0) {
-						flag = 1;
-						System.out.println("NO");
-						break;
-					}
-					if (!paranthesis[size - 1].equals("[")) {
-						System.out.println("NO");
-						flag = 1;
-						break;
-					}
-					size--;
-				}
-			}
-			if (size == 0 && flag == 0) {
-				System.out.println("YES");
-			} else if (size > 0 && flag == 0) {
-				System.out.println("NO");
-			}
-		}
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testnum = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testnum; i++) {
+            String[] paranthesis = new String[20];
+            String line = scan.nextLine();
+            String[] tokens = line.split("");
+            int size = 0;
+            int flag = 0;
+            for (int j = 0; j < tokens.length; j++) {
+                if (tokens[j].equals("(") || tokens[j].equals("{") || tokens[j].equals("[")) {
+                    if (size == paranthesis.length) {
+                        paranthesis = Arrays.copyOf(paranthesis, 2 * size);
+                    }
+                    paranthesis[size] = tokens[j];
+                    size++;
+                } else if (tokens[j].equals("}")) {
+                    if (size == 0) {
+                        flag = 1;
+                        System.out.println("NO");
+                        break;
+                    }
+                    if (!paranthesis[size - 1].equals("{")) {
+                        System.out.println("NO");
+                        flag = 1;
+                        break;
+                    }
+                    size--;
+                } else if (tokens[j].equals(")")) {
+                    if (size == 0) {
+                        flag = 1;
+                        System.out.println("NO");
+                        break;
+                    }
+                    if (!paranthesis[size - 1].equals("(")) {
+                        System.out.println("NO");
+                        flag = 1;
+                        break;
+                    }
+                    size--;
+                } else if (tokens[j].equals("]")) {
+                    if (size == 0) {
+                        flag = 1;
+                        System.out.println("NO");
+                        break;
+                    }
+                    if (!paranthesis[size - 1].equals("[")) {
+                        System.out.println("NO");
+                        flag = 1;
+                        break;
+                    }
+                    size--;
+                }
+            }
+            if (size == 0 && flag == 0) {
+                System.out.println("YES");
+            } else if (size > 0 && flag == 0) {
+                System.out.println("NO");
+            }
+        }
+    }
 }
-
-
