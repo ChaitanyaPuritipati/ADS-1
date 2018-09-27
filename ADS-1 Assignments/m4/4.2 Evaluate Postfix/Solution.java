18432cca25874998b6ed0b64f6632c20418320d5
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-27 15:39:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 Assignments/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1 Assignments/m4/4.2 Evaluate Postfix/Solution.java
@@ -4,44 +4,52 @@ import java.util.LinkedList;
  * Class for solution.
  */
 class Solution {
-	Solution() {
-		//unused
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String line = scan.nextLine();
-		String[] tokens = line.split(" ");
-		String str = "+*-/";
-		LinkedList<Integer> operands = new LinkedList<Integer>();
-		for (int i = 0; i < tokens.length; i++) {
-			if (!str.contains(tokens[i])) {
-				operands.add(Integer.parseInt(tokens[i]));
-			} else {
-				switch (tokens[i]) {
-				case "+":
-					int a = operands.removeLast();
-					int b = operands.removeLast();
-					operands.add(a + b);
-					break;
-				case "-":
-					a = operands.removeLast();
-					b = operands.removeLast();
-					operands.add(a - b);
-					break;
-				case "*":
-					a = operands.removeLast();
-					b = operands.removeLast();
-					operands.add(a * b);
-					break;
-				case "/":
-					a = operands.removeLast();
-					b = operands.removeLast();
-					operands.add(a / b);
-					break;
-				default:
-				}
-			}
-		}
-		System.out.println(operands.get(0));
-	}
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+        //unused
+    }
+    /**
+     * { Main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        String[] tokens = line.split(" ");
+        String str = "+*-/";
+        LinkedList<Integer> operands = new LinkedList<Integer>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (!str.contains(tokens[i])) {
+                operands.add(Integer.parseInt(tokens[i]));
+            } else {
+                switch (tokens[i]) {
+                case "+":
+                    int a = operands.removeLast();
+                    int b = operands.removeLast();
+                    operands.add(a + b);
+                    break;
+                case "-":
+                    a = operands.removeLast();
+                    b = operands.removeLast();
+                    operands.add(a - b);
+                    break;
+                case "*":
+                    a = operands.removeLast();
+                    b = operands.removeLast();
+                    operands.add(a * b);
+                    break;
+                case "/":
+                    a = operands.removeLast();
+                    b = operands.removeLast();
+                    operands.add(a / b);
+                    break;
+                default:
+                }
+            }
+        }
+        System.out.println(operands.get(0));
+    }
 }
