c316f75037ebbd1cbf76875a18bdd947ce9039f3
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-27 10:38:54 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- ADS-1 Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1 Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -39,13 +39,17 @@ class Solution {
 					size++;
 				} else if (Character.toString(line.charAt(j)).equals(")") && size > 0) {
 					size--;
+				} else if(Character.toString(line.charAt(j)).equals(")") && size == 0) {
+					System.out.println("NO");
+					flag = 1;
+					break;
 				}
-		}
-		if (size == 0 && flag == 0) {
-			System.out.println("YES");
-		} else if (size > 0 && flag == 0) {
-			System.out.println("NO");
+			}
+			if (size == 0 && flag == 0) {
+				System.out.println("YES");
+			} else if (size > 0 && flag == 0) {
+				System.out.println("NO");
+			}
 		}
 	}
-}
 }
