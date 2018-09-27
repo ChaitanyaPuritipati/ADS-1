8b54ee75250379602e551a9b5ee711146c7f0dcf
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-27 15:15:34 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1 Assignments/m4/4.2 Evaluate Postfix/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+class Solution {
+	Solution() {
+		//unused
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String line = scan.nextLine();
+		String[] tokens = line.split("");
+		LinkedList<Integer> operands = new LinkedList<Integer>();
+		// int c = 0;
+		for (int i = 0; i < tokens.length; i++) {
+			if (!"*/-+".contains(tokens[i])) {
+				operands.add(Integer.parseInt(tokens[i]));
+			} else {
+				switch (tokens[i]) {
+				case "+":
+					int a = operands.removeLast();
+					int b = operands.removeLast();
+					// c = c + a + b;
+					operands.add(a + b);
+					break;
+				case "-":
+					a = operands.removeLast();
+					b = operands.removeLast();
+					operands.add(a - b);
+					break;
+				case "*":
+				    a = operands.removeLast();
+				    b = operands.removeLast();
+				    operands.add(a * b);
+				    break;
+				case "/":
+				    a = operands.removeLast();
+				    b = operands.removeLast();
+				    operands.add(a / b);    	
+                    break;
+                default:    
+				}
+			}
+		}
+		System.out.println(operands.get(0));
+	}
+}
