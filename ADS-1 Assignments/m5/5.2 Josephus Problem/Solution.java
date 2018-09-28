27f5233faccf7dd63443f4e781a9919bff1c4acf
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-28 17:29:28 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- ADS-1 Assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -6,22 +6,27 @@ class Josephus {
 	int skipval;
 	Josephus(int inputsize, int inputskipval) {
 		this.seats = new int[inputsize];
-	    this.size = inputsize;
-		for(int l = 0; l < this.size; l++) {
+		this.size = inputsize;
+		for (int l = 0; l < this.size; l++) {
 			seats[l] = l;
 		}
 		this.skipval = inputskipval;
 	}
 	void removing() {
 		int i = 0;
+
 		while (this.size > 0) {
+			int rem = 0;
 			// System.out.println(size + "sizeval");
-			if(i == (this.size -1)) {
+			if (i == (this.size - 1)) {
 				i = -1;
 			}
-			int rem = (i + skipval)%size;
+			while(i + skipval - 1 < size) {
+				 System.out.println("hellp");
+				 rem = (i + skipval) % size;
+			}
 			System.out.print(seats[i + skipval - 1] + " ");
-			for(int j = i + skipval - 1; j < this.size - 1; j++) {
+			for (int j = i + skipval - 1; j < this.size - 1; j++) {
 				seats[j] = seats[j + 1];
 			}
 			// System.out.println(Arrays.toString(seats));
@@ -29,19 +34,19 @@ class Josephus {
 			this.size--;
 			// System.out.println(this.size + "later");
 		}
+		System.out.println();
 	}
 
 }
 class Solution {
-	Solution(){
+	Solution() {
 		//unused;
 	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int testnum = Integer.parseInt(scan.nextLine());
-		for(int k = 0; k < testnum; k++) {
+		for (int k = 0; k < testnum; k++) {
 			String[] tokens = scan.nextLine().split(" ");
-			// System.out.println(Arrays.toString(tokens));
 			Josephus start = new Josephus(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 			start.removing();
 		}
