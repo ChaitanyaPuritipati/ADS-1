62d077b197674a327ff9010216dcb76bdc24ddf2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-28 16:51:35 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ ADS-1 Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Josephus {
+	int[] seats;
+	int size;
+	int skipval;
+	Josephus(int inputsize, int inputskipval) {
+		this.seats = new int[inputsize];
+		this.size = inputsize;
+		for(int l = 0; l < this.size; l++) {
+			seats[l] = l;
+		}
+		this.skipval = inputskipval;
+	}
+	void removing() {
+		int i = 0;
+		while (size != 0) {
+			if(i == size -1) {
+				i = -1;
+			}
+			System.out.println(seats[i + skipval - 1]);
+			for(int j = i + skipval - 1; j < size - 1; j++) {
+				seats[j] = seats[j + 1];
+			}
+			i = i + skipval - 1;
+			size--;
+		}
+	}
+
+}
+class Solution {
+	Solution(){
+		//unused;
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testnum = Integer.parseInt(scan.nextLine());
+		for(int k = 0; k < testnum; k++) {
+			String[] tokens = scan.nextLine().split(" ");
+			Josephus start = new Josephus(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+			start.removing();
+		}
+	}
+}
