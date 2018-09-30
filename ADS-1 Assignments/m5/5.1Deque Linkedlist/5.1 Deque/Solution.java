db251846becae1ecb3b3ddc84a05cbd4a94bb4c1
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-30 10:21:11 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- ADS-1 Assignments/m5/5.1Deque Linkedlist/5.1 Deque/Solution.java
+++ ADS-1 Assignments/m5/5.1Deque Linkedlist/5.1 Deque/Solution.java
@@ -1,31 +1,42 @@
 import java.util.Scanner;
-class Solution {
-	Solution() {
-	}
-	public static void main(String[] args) {
-		Deque<Integer> dequeobj = new Deque<Integer>();
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "pushLeft":
-				dequeobj.pushLeft(Integer.parseInt(tokens[1]));
-				break;
-			case "pushRight":
-				dequeobj.pushRight(Integer.parseInt(tokens[1]));
-				break;
-			case "size":
-				System.out.println(dequeobj.size());
-				break;
-			case "popRight":
-				dequeobj.popRight();
-				break;
-			case "popLeft":
-				dequeobj.popLeft();
-				break;
-			default:
-			}
-		}
-	}
+/**
+ * { Solution }.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { Main method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Deque<Integer> dequeobj = new Deque<Integer>();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "pushLeft":
+                dequeobj.pushLeft(Integer.parseInt(tokens[1]));
+                break;
+            case "pushRight":
+                dequeobj.pushRight(Integer.parseInt(tokens[1]));
+                break;
+            case "size":
+                System.out.println(dequeobj.size());
+                break;
+            case "popRight":
+                dequeobj.popRight();
+                break;
+            case "popLeft":
+                dequeobj.popLeft();
+                break;
+            default:
+            }
+        }
+    }
 }
