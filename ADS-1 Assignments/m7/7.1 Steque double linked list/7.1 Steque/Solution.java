25f0c2308b23c70e69584c4546c3cfa2cd8534ff
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-01 11:14:42 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

---
+++ ADS-1 Assignments/m7/7.1 Steque double linked list/7.1 Steque/Solution.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+class Steque {
+	Node head = null;
+	Node tail = null;
+	int size = 0;
+	class Node {
+		int nodedata;
+		Node prevnode;
+		Node nextnode;
+		Node(int data) {
+			nodedata = data;
+		}
+	}
+	boolean isEmpty() {
+		return size == 0;
+	}
+	void push(int item) {
+		Node newnode = new Node(item);
+		if (isEmpty()) {
+			newnode.nextnode = head;
+			newnode.prevnode = head;
+			head = newnode;
+			tail = head;
+			size++;
+			print();
+			return;
+		}
+		newnode.nextnode = head;
+		newnode.prevnode = head.prevnode;
+		head.prevnode = newnode;
+		head = newnode;
+		size++;
+		print();
+	}
+	void enqueue(int item) {
+		Node newnode = new Node(item);
+		if (isEmpty()) {
+			newnode.nextnode = tail;
+			newnode.prevnode = tail;
+			tail = newnode;
+			head = tail;
+			size++;
+			print();
+			return;
+		}
+		newnode.nextnode = tail.nextnode;
+		newnode.prevnode = tail;
+		tail.nextnode = newnode;
+		tail = newnode;
+		size++;
+		print();
+	}
+	void pop() {
+		if (isEmpty()) {
+			System.out.println("Steque is empty.");
+			return;
+		}
+		int result = head.nodedata;
+		head = head.nextnode;
+		size--;
+		print();
+	}
+	void print() {
+		if (isEmpty()) {
+			System.out.println("Steque is empty.");
+			return;
+		}
+		String str = "";
+		Node test = head;
+		while (test.nextnode != null) {
+			str = str + test.nodedata + ", ";
+			test = test.nextnode;
+		}
+		str = str + tail.nodedata;
+		System.out.println(str);
+	}
+}
+class Solution {
+	Solution() {
+		//unused
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testnum = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < testnum; i++) {
+			Steque steque = new Steque();
+			while (scan.hasNext()) {
+				String line = scan.nextLine();
+				if (!line.isEmpty()) {
+					String[] tokens = line.split(" ");
+					switch (tokens[0]) {
+					case "push":
+						steque.push(Integer.parseInt(tokens[1]));
+						break;
+					case "enqueue":
+						steque.enqueue(Integer.parseInt(tokens[1]));
+						break;
+					case "pop":
+						if (steque.isEmpty()) {
+							System.out.println("Steque is empty.");
+							break;
+						}
+						steque.pop();
+						break;
+					default:
+						break;
+					}
+				} else {
+					System.out.println();
+					break;
+				}
+			}
+		}
+	}
+}
