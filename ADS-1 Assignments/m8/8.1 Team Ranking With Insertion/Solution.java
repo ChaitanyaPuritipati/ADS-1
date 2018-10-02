c879b73657f1b23d9e594d87961604306b3bac1e
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-02 19:44:15 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS-1 Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class insertionSort {
+
+	// boolean less( inputone, Comparable inputtwo) {
+	// 	return inputone.compareTo(inputtwo) == -1;
+	// }
+
+	boolean greater(teamdata inputone, teamdata inputtwo) {
+		return inputone.compareTo(inputtwo) == 1;
+	}
+
+	void exchange(teamdata[] inputarray, int bigindex, int smallindex) {
+		teamdata temp = inputarray[bigindex];
+		inputarray[bigindex] = inputarray[smallindex];
+		inputarray[smallindex] = temp;
+	}
+
+	void Sort(teamdata[] inputarray) {
+		int length = inputarray.length;
+		for (int i = 1; i < length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (greater(inputarray[j], inputarray[j - 1])) {
+					exchange(inputarray, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(inputarray));
+	}
+}
+class teamdata implements Comparable<teamdata> {
+	String teamname;
+	int teamwins;
+	int teamlosses;
+	int teamdraws;
+	teamdata(String name, int wins, int losses, int draws) {
+		this.teamname = name;
+		this.teamwins = wins;
+		this.teamlosses = losses;
+		this.teamdraws = draws;
+	}
+	public int compareTo(teamdata other) {
+		if (this.teamwins > other.teamwins) {
+			return 1;
+		}
+		if (this.teamwins < other.teamwins) {
+			return -1;
+		}
+		if (this.teamlosses > other.teamlosses) {
+			return 1;
+		}
+		if (this.teamlosses < other.teamlosses) {
+			return -1;
+		}
+		if (this.teamdraws > other.teamdraws) {
+			return 1;
+		}
+		if (this.teamdraws < other.teamdraws) {
+			return -1;
+		}
+		return 0;
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		teamdata[] data = new teamdata[10];
+		int count = 0;
+		insertionSort startobj = new insertionSort();
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(",");
+			teamdata everydata = new teamdata(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+			data[count] = everydata;
+			count++;
+		}
+		data = Arrays.copyOf(data, count);
+		startobj.Sort(data);
+	}
+}
