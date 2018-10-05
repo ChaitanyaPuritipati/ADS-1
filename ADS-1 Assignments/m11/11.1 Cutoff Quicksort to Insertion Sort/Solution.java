6733d6dbbdb2d73a7b36ffaee9c5bf5d1fe52614
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-05 20:25:10 +0530

    "ads1_m11_a1_v2: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
+++ ADS-1 Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
@@ -131,6 +131,7 @@ class Solution {
 			int cutoffval = Integer.parseInt(scan.nextLine());
 			Comparable[] inputarray = scan.nextLine().split(" ");
 			quick.sort(inputarray, 0, inputarray.length - 1, cutoffval);
+			System.out.println("---------------------------------------");
 		}
 	}
 }
