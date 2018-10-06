9a4d0f85ea64c6fdfdcd79dcb1806defb85bd5fe
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-06 20:50:32 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/Module 12/m12/Assignment-1/Insertionsort.java
+++ ADS-1 Assignments/Module 12/m12/Assignment-1/Insertionsort.java
@@ -1,14 +1,46 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for insertionsort.
+ */
 class Insertionsort {
+	/**
+	 * { exchanges two items }.
+	 * Complexity: Best: O(1)
+	 *             Worst: O(1)
+	 *             Average: O(1)
+	 *
+	 * @param      array       The array
+	 * @param      smallindex  The smallindex
+	 * @param      bigindex    The bigindex
+	 */
 	void exchange(studentdata[] array, int smallindex, int bigindex) {
 		studentdata temp = array[smallindex];
 		array[smallindex] = array[bigindex];
 		array[bigindex] = temp;
 	}
+	/**
+	 * { To check min element }.
+	 * Complexity: Best: O(1)
+	 *             Worst: O(1)
+	 *             Average: O(1)
+	 *
+	 * @param      valueone  The valueone
+	 * @param      valuetwo  The valuetwo
+	 *
+	 * @return     { boolean }
+	 */
 	boolean less(studentdata valueone, studentdata valuetwo) {
 		return valueone.compareTo(valuetwo) == -1;
 	}
+	/**
+	 * { Insertion sort function }.
+	 * Complexity: Best: O(N^2)
+	 *             Worst: O(N^2)
+	 *             Average: O(N^2)
+	 *
+	 * @param      students  The students
+	 */
 	void sort(studentdata[] students) {
 		for (int i = 1; i < students.length; i++) {
 			for (int j = i; j > 0; j--) {
@@ -21,14 +53,49 @@ class Insertionsort {
 		}
 	}
 }
+/**
+ * Class for studentdata.
+ */
 class studentdata implements Comparable<studentdata> {
+	/**
+	 * { var_description }
+	 */
 	String studentname;
+	/**
+	 * { var_description }
+	 */
 	String birthdate;
+	/**
+	 * { var_description }
+	 */
 	int sub1marks;
+	/**
+	 * { var_description }
+	 */
 	int sub2marks;
+	/**
+	 * { var_description }
+	 */
 	int sub3marks;
+	/**
+	 * { var_description }
+	 */
 	int totalmarks;
+	/**
+	 * { var_description }
+	 */
 	String rescategory;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name      The name
+	 * @param      dob       The dob
+	 * @param      sub1      The sub 1
+	 * @param      sub2      The sub 2
+	 * @param      sub3      The sub 3
+	 * @param      total     The total
+	 * @param      category  The category
+	 */
 	studentdata(String name, String dob, int sub1, int sub2, int sub3, int total, String category) {
 		this.studentname = name;
 		this.birthdate = dob;
@@ -38,10 +105,28 @@ class studentdata implements Comparable<studentdata> {
 		this.totalmarks = total;
 		this.rescategory = category;
 	}
+	/**
+	 * { To split the data string }.
+	 * Complexity: Best: O(1)
+	 *             Worst: O(1)
+	 *             Average: O(1)
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	String[] datesplit() {
 		String[] splitdate = birthdate.split("-");
 		return splitdate;
 	}
+	/**
+	 * { CompareTo function }.
+	 * Complexity: Best: O(1)
+	 *             Worst: O(1)
+	 *             Average: O(1)
+	 *
+	 * @param      other  The other
+	 *
+	 * @return     { int }
+	 */
 	public int compareTo(studentdata other) {
 		if (this.totalmarks > other.totalmarks) {
 			return 1;
