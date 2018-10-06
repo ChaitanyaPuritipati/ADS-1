71c76940de1677462a54c8567afb9be206a1c8e3
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-06 11:02:48 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 Assignments/Module 12/m12/Assignment-1/Solution.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+class Insertionsort {
+	void exchange(studentdata[] array, int smallindex, int bigindex) {
+		studentdata temp = array[smallindex];
+		array[smallindex] = array[bigindex];
+		array[bigindex] = temp;
+	}
+	boolean less(studentdata valueone, studentdata valuetwo) {
+		return valueone.compareTo(valuetwo) == -1;
+	}
+	void sort(studentdata[] students) {
+		for (int i = 1; i < students.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (less(students[j - 1], students[j])) {
+					exchange(students, j - 1, j);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+}
+class studentdata implements Comparable<studentdata> {
+	String studentname;
+	String birthdate;
+	int sub1marks;
+	int sub2marks;
+	int sub3marks;
+	int totalmarks;
+	String rescategory;
+	studentdata(String name, String dob, int sub1, int sub2, int sub3, int total, String category) {
+		this.studentname = name;
+		this.birthdate = dob;
+		this.sub1marks = sub1;
+		this.sub2marks = sub2;
+		this.sub3marks = sub3;
+		this.totalmarks = total;
+		this.rescategory = category;
+	}
+	String[] datesplit() {
+		String[] splitdate = birthdate.split("-");
+		return splitdate;
+	}
+	public int compareTo(studentdata other) {
+		if (this.totalmarks > other.totalmarks) {
+			return 1;
+		}
+		if (this.totalmarks < other.totalmarks) {
+			return -1;
+		}
+		if (this.sub3marks > other.sub3marks) {
+			return 1;
+		}
+		if (this.sub3marks < other.sub3marks) {
+			return -1;
+		}
+		if (this.sub2marks > other.sub2marks) {
+			return 1;
+		}
+		if (this.sub2marks < other.sub2marks) {
+			return -1;
+		}
+		if (Integer.parseInt(this.datesplit()[2]) > Integer.parseInt(other.datesplit()[2])) {
+			return 1;
+		}
+		if (Integer.parseInt(this.datesplit()[2]) < Integer.parseInt(other.datesplit()[2])) {
+			return -1;
+		}
+		if (Integer.parseInt(this.datesplit()[1]) > Integer.parseInt(other.datesplit()[1])) {
+			return 1;
+		}
+		if (Integer.parseInt(this.datesplit()[1]) < Integer.parseInt(other.datesplit()[1])) {
+			return -1;
+		}
+		if (Integer.parseInt(this.datesplit()[0]) > Integer.parseInt(other.datesplit()[0])) {
+			return 1;
+		}
+		if (Integer.parseInt(this.datesplit()[0]) < Integer.parseInt(other.datesplit()[0])) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
+class Solution {
+	Solution() {
+		//unused
+	}
+	public static void print(studentdata[] array) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i].studentname + "," +
+				array[i].totalmarks + "," +
+				array[i].rescategory);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		int num = Integer.parseInt(scan.nextLine());
+		studentdata[] students = new studentdata[num];
+		int vacancynum = Integer.parseInt(scan.nextLine());
+		int openvacancy = Integer.parseInt(scan.nextLine());
+		int bcvacancy = Integer.parseInt(scan.nextLine());
+		int scvacancy = Integer.parseInt(scan.nextLine());
+		int stvacancy = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < num; i++) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(",");
+			studentdata eachstudentdata = new studentdata(tokens[0], tokens[1],
+			        Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
+			        Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), tokens[6]);
+			students[i] = eachstudentdata;
+		}
+		Insertionsort insertion = new Insertionsort();
+		insertion.sort(students);
+		print(students);
+	}
+
+}
