73668b0a9855906dcf6bf434ca159782a5310019
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-06 12:13:09 +0530

    "ads1_m12_a1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/Module 12/m12/Assignment-1/Solution.java
+++ ADS-1 Assignments/Module 12/m12/Assignment-1/Solution.java
@@ -86,6 +86,26 @@ class Solution {
 	Solution() {
 		//unused
 	}
+	static int opencount = 0;
+	static int bccount = 0;
+	static int sccount = 0;
+	static int stcount = 0;
+	public static void countcat(studentdata[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].rescategory.equals("Open")) {
+				opencount++;
+			}
+			if (array[i].rescategory.equals("BC")) {
+				bccount++;
+			}
+			if (array[i].rescategory.equals("SC")) {
+				sccount++;
+			}
+			if (array[i].rescategory.equals("ST")) {
+				stcount++;
+			}
+		}
+	}
 	public static void meritfill(studentdata[] array, int vacancy, int open, int bc, int sc, int st) {
 		for (int i = 0; i < array.length; i++) {
 			if (vacancy == 0) {
@@ -126,48 +146,46 @@ class Solution {
 						                   array[i].rescategory);
 						st--;
 						vacancy--;
+					} else if (bccount == 0 && sccount == 0 && stcount == 0) {
+						System.out.println(array[i].studentname + "," +
+						                   array[i].totalmarks + "," +
+						                   array[i].rescategory);
+						vacancy--;
 					}
-				} else if(vacancy > 0 && sc <= 0 && bc <= 0 && st <= 0) {
-					System.out.println(array[i].studentname + "," +
-					                   array[i].totalmarks + "," +
-					                   array[i].rescategory);
-					vacancy--;
 				}
 			}
+		}
+	}
 
-		
+	public static void print(studentdata[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i].studentname + "," +
+			                   array[i].totalmarks + "," +
+			                   array[i].rescategory);
+		}
 	}
-}
-
-public static void print(studentdata[] array) {
-	for (int i = 0; i < array.length; i++) {
-		System.out.println(array[i].studentname + "," +
-		                   array[i].totalmarks + "," +
-		                   array[i].rescategory);
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		int num = Integer.parseInt(scan.nextLine());
+		studentdata[] students = new studentdata[num];
+		int vacancynum = Integer.parseInt(scan.nextLine());
+		int openvacancy = Integer.parseInt(scan.nextLine());
+		int bcvacancy = Integer.parseInt(scan.nextLine());
+		int scvacancy = Integer.parseInt(scan.nextLine());
+		int stvacancy = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < num; i++) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(",");
+			studentdata eachstudentdata = new studentdata(tokens[0], tokens[1],
+			        Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
+			        Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), tokens[6]);
+			students[i] = eachstudentdata;
+		}
+		Insertionsort insertion = new Insertionsort();
+		insertion.sort(students);
+		print(students);
+		System.out.println();
+		meritfill(students, vacancynum, openvacancy, bcvacancy, scvacancy, stvacancy);
 	}
-}
-public static void main(String[] args) {
-	Scanner scan =  new Scanner(System.in);
-	int num = Integer.parseInt(scan.nextLine());
-	studentdata[] students = new studentdata[num];
-	int vacancynum = Integer.parseInt(scan.nextLine());
-	int openvacancy = Integer.parseInt(scan.nextLine());
-	int bcvacancy = Integer.parseInt(scan.nextLine());
-	int scvacancy = Integer.parseInt(scan.nextLine());
-	int stvacancy = Integer.parseInt(scan.nextLine());
-	for (int i = 0; i < num; i++) {
-		String line = scan.nextLine();
-		String[] tokens = line.split(",");
-		studentdata eachstudentdata = new studentdata(tokens[0], tokens[1],
-		        Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
-		        Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), tokens[6]);
-		students[i] = eachstudentdata;
-	}
-	Insertionsort insertion = new Insertionsort();
-	insertion.sort(students);
-	print(students);
-	System.out.println();
-	meritfill(students, vacancynum, openvacancy, bcvacancy, scvacancy, stvacancy);
-}
 
 }
