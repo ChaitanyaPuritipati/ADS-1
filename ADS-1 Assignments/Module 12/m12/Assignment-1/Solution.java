870c117c3f5796355a22438e9e66b04c5eec293f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-06 11:56:01 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/Module 12/m12/Assignment-1/Solution.java
+++ ADS-1 Assignments/Module 12/m12/Assignment-1/Solution.java
@@ -89,6 +89,7 @@ class Solution {
 	public static void meritfill(studentdata[] array, int vacancy, int open, int bc, int sc, int st) {
 		for (int i = 0; i < array.length; i++) {
 			if (vacancy == 0) {
+				System.out.println("entered");
 				return;
 			}
 			if (open > 0) {
