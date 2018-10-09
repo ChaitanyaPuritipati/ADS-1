88c6e66c0bb621802dc88891b613627fa90c5945
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-09 19:21:23 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 Assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused
+	}
+	/**
+	 * { main function }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("MAXHEAP");
+		System.out.println("MINHEAP");
+	}
+}
