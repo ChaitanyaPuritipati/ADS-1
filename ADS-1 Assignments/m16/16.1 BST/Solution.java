145d7ada5fcd3d79bb7d9e55c7356c61bfde081d
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-11 22:29:07 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/m16/16.1 BST/Solution.java
+++ ADS-1 Assignments/m16/16.1 BST/Solution.java
@@ -3,40 +3,40 @@ import java.util.Scanner;
  * Class for key.
  */
 class Key implements Comparable<Key> {
-	/**
-	 * { Name of the book }.
-	 */
-	private String bookname;
-	/**
-	 * { Author of the book }.
-	 */
-	private String bookauthor;
-	/**
-	 * { Cost of the book }.
-	 */
-	private String bookcost;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      name    The name
-	 * @param      author  The author
-	 * @param      cost    The cost
-	 */
-	Key(String name, String author, String cost) {
-		this.bookname = name;
-		this.bookauthor = author;
-		this.bookcost = cost;
-	}
-	/**
-	 * { To compare names }.
-	 *
-	 * @param      other  The other
-	 *
-	 * @return     { int value }
-	 */
-	public int compareTo(Key other) {
-		return this.bookname.compareTo(other.bookname);
-	}
+    /**
+     * { Name of the book }.
+     */
+    private String bookname;
+    /**
+     * { Author of the book }.
+     */
+    private String bookauthor;
+    /**
+     * { Cost of the book }.
+     */
+    private String bookcost;
+    /**
+     * Constructs the object.
+     *
+     * @param      name    The name
+     * @param      author  The author
+     * @param      cost    The cost
+     */
+    Key(String name, String author, String cost) {
+        this.bookname = name;
+        this.bookauthor = author;
+        this.bookcost = cost;
+    }
+    /**
+     * { To compare names }.
+     *
+     * @param      other  The other
+     *
+     * @return     { int value }
+     */
+    public int compareTo(Key other) {
+        return this.bookname.compareTo(other.bookname);
+    }
 }
 /**
  * Class for binary search tree.
@@ -45,109 +45,154 @@ class Key implements Comparable<Key> {
  * @param      <Value>  The value
  */
 class BinarySearchTree<Key extends Comparable<Key>, Value> {
-	private Node head = null;
-	private int size = 0;
-	class Node {
-		private Node left;
-		private Node right;
-		private Key keydata;
-		private Value valdata;
-		Node(Key keyval, Value val) {
-			this.keydata = keyval;
-			this.valdata = val;
-		}
-	}
-	boolean isEmpty() {
-		return size == 0;
-	}
-	void put(Key item, Value itemval) {
-		Node newnode = new Node(item, itemval);
-		if (isEmpty()) {
-			newnode.left = null;
-			newnode.right = null;
-			head = newnode;
-			size++;
-			return;
-		}
-		Node test = head;
-		while (!(test.left == null && test.right == null)) {
-			int compare = item.compareTo(test.keydata);
-			if (compare > 0) {
-				if (test.right == null) {
-					break;
-				}
-				test = test.right;
-			} else if (compare < 0) {
-				if (test.left == null) {
-					break;
-				}
-				test = test.left;
-			} else {
-				test.valdata = newnode.valdata;
-				return;
-			}
-		}
-		int compare = item.compareTo(test.keydata);
-		if (compare == 0) {
-			test.valdata = newnode.valdata;
-			return;
-		} else if (compare < 0) {
-			test.left = newnode;
-			test.left.left = null;
-			test.left.right = null;
-			size++;
-		} else if (compare > 0) {
-			test.right = newnode;
-			test.right.left = null;
-			test.right.right = null;
-			size++;
-		}
-	}
-	Value get(Key item) {
-		Node test = head;
-		while (!(test.left == null && test.right == null)) {
-			int compare = item.compareTo(test.keydata);
-			if (compare > 0) {
-				if (test.right == null) {
-					break;
-				}
-				test = test.right;
-			} else if (compare < 0) {
-				if (test.left == null) {
-					break;
-				}
-				test = test.left;
-			} else {
-				break;
-			}
-		}
-		if (item.compareTo(test.keydata) == 0) {
-			return test.valdata;
-		}
-		return null;
-	}
+    /**
+     * { Head Node }.
+     */
+    private Node head = null;
+    /**
+     * { Size value }.
+     */
+    private int size = 0;
+    /**
+     * Class for node.
+     */
+    class Node {
+        /**
+         * { Left node }.
+         */
+        private Node left;
+        /**
+         * { Right node }.
+         */
+        private Node right;
+        /**
+         * { Keydata }.
+         */
+        private Key keydata;
+        /**
+         * { valuedata }.
+         */
+        private Value valdata;
+        /**
+         * Constructs the object.
+         *
+         * @param      keyval  The keyval
+         * @param      val     The value
+         */
+        Node(Key keyval, Value val) {
+            this.keydata = keyval;
+            this.valdata = val;
+        }
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    boolean isEmpty() {
+        return size == 0;
+    }
+    /**
+     * { Put function }.
+     *
+     * @param      item     The item
+     * @param      itemval  The itemval
+     */
+    void put(Key item, Value itemval) {
+        Node newnode = new Node(item, itemval);
+        if (isEmpty()) {
+            newnode.left = null;
+            newnode.right = null;
+            head = newnode;
+            size++;
+            return;
+        }
+        Node test = head;
+        while (!(test.left == null && test.right == null)) {
+            int compare = item.compareTo(test.keydata);
+            if (compare > 0) {
+                if (test.right == null) {
+                    break;
+                }
+                test = test.right;
+            } else if (compare < 0) {
+                if (test.left == null) {
+                    break;
+                }
+                test = test.left;
+            } else {
+                test.valdata = newnode.valdata;
+                return;
+            }
+        }
+        int compare = item.compareTo(test.keydata);
+        if (compare == 0) {
+            test.valdata = newnode.valdata;
+            return;
+        } else if (compare < 0) {
+            test.left = newnode;
+            // test.left.left = null;
+            // test.left.right = null;
+            size++;
+        } else if (compare > 0) {
+            test.right = newnode;
+            // test.right.left = null;
+            // test.right.right = null;
+            size++;
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
+     */
+    Value get(Key item) {
+        Node test = head;
+        while (!(test.left == null && test.right == null)) {
+            int compare = item.compareTo(test.keydata);
+            if (compare > 0) {
+                if (test.right == null) {
+                    break;
+                }
+                test = test.right;
+            } else if (compare < 0) {
+                if (test.left == null) {
+                    break;
+                }
+                test = test.left;
+            } else {
+                break;
+            }
+        }
+        if (item.compareTo(test.keydata) == 0) {
+            return test.valdata;
+        }
+        return null;
+    }
 }
 class Solution {
-	private Solution() {
-		//unused.
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		BinarySearchTree<Key, Integer> bstobj = new BinarySearchTree<>();
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(",");
-			switch (tokens[0]) {
-			case "put":
-				Key newkey = new Key(tokens[1], tokens[2], tokens[3]);
-				bstobj.put(newkey, Integer.parseInt(tokens[4]));
-				break;
-			case "get":
-				newkey = new Key(tokens[1], tokens[2], tokens[3]);
-				System.out.println(bstobj.get(newkey));
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    private Solution() {
+        //unused.
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinarySearchTree<Key, Integer> bstobj = new BinarySearchTree<>();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            switch (tokens[0]) {
+            case "put":
+                Key newkey = new Key(tokens[1], tokens[2], tokens[3]);
+                bstobj.put(newkey, Integer.parseInt(tokens[4]));
+                break;
+            case "get":
+                newkey = new Key(tokens[1], tokens[2], tokens[3]);
+                System.out.println(bstobj.get(newkey));
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
