377ee24a86fdf9d16ec9204cd3a47642f1a12746
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-11 09:35:38 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Practice/practice/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1 Practice/practice/15.1 BinarySearchSymbolTable/Solution.java
@@ -2,15 +2,42 @@ import java.util.Scanner;
 import java.util.Iterator;
 import java.util.Arrays;
 import java.util.List;
+/**
+ * Class for binary search tree.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
 class BinarySearchTree<Key extends Comparable<Key>, Value> {
+	/**
+	 * { var_description }.
+	 */
 	Key[] keys;
+	/**
+	 * { var_description }.
+	 */
 	Value[] values;
+	/**
+	 * { var_description }.
+	 */
 	int size;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      length  The length
+	 */
 	BinarySearchTree(int length) {
 		keys = (Key[])new Comparable[length];
 		values = (Value[]) new Object[length];
 		size = 0;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      keyval  The keyval
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	int rank(Key keyval) {
 		int low = 0;
 		int high = size - 1;
@@ -27,6 +54,13 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 		return low;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      keyval  The keyval
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	Value get(Key keyval) {
 		if (!contains(keyval)) {
 			return null;
@@ -34,13 +68,31 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		int rankval = rank(keyval);
 		return values[rankval];
 	}
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
 	boolean isEmpty() {
 		return size == 0;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      keyval  The keyval
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	boolean contains(Key keyval) {
 		int rankval = rank(keyval);
 		return keyval.compareTo(keys[rankval]) == 0;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      keyval  The keyval
+	 * @param      val     The value
+	 */
 	void put(Key keyval, Value val) {
 		int rankval = rank(keyval);
 		if (size > 0 && rankval < size && keys[rankval].compareTo(keyval) == 0) {
@@ -55,9 +107,17 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		values[rankval] = val;
 		size++;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	Key max() {
 		return keys[size - 1];
 	}
+	/**
+	 * { function_description }.
+	 */
 	void deleteMin() {
 		for (int i = 0; i < size - 1; i++) {
 			keys[i] = keys[i + 1];
@@ -65,12 +125,13 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 		size--;
 	}
-	// void print() {
-	// 	for (int i = 0; i < size; i++) {
-	// 		System.out.println(keys[i] + " " + values[i]);
-	// 	}
-	// }
-
+	/**
+	 * { function_description }.
+	 *
+	 * @param      keyval  The keyval
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	Key floor(Key keyval) {
 		int rankval = rank(keyval);
 		if (rankval < size && contains(keyval)) {
@@ -81,6 +142,9 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 		return keys[rankval - 1];
 	}
+	/**
+	 * { function_description }.
+	 */
 	void print() {
 		Key[] keysnew = Arrays.copyOf(keys, size);
 		MyIterable<Key> mylist = new MyIterable<>(keysnew);
@@ -89,19 +153,48 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 	}
 }
+/**
+ * Class for my iterable.
+ *
+ * @param      <Key>  The key
+ */
 class MyIterable<Key> implements Iterable<Key> {
+	/**
+	 * { var_description }.
+	 */
 	private List<Key> list;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      k     { parameter_description }
+	 */
 	public MyIterable(Key[] k) {
 		list = Arrays.asList(k);
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public Iterator<Key> iterator() {
 		return list.iterator();
 	}
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
-
+		//unused.
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] array = scan.nextLine().split(" ");
