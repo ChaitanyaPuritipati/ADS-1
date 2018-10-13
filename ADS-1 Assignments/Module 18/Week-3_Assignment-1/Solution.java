6a70ba62917d2ee3d2b956bbf5f2810e51b8736c
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-13 11:00:04 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 Assignments/Module 18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+class Stock implements Comparable<Stock> {
+	private String stockname;
+	private float stockchange;
+	Stock(String name, float change) {
+		this.stockname = name;
+		this.stockchange = change;
+	}
+	public String getstockname() {
+		return this.stockname;
+	}
+	public float getstockchange() {
+		return this.stockchange;
+	}
+	public int compareTo(Stock other) {
+		if (this.stockchange > other.stockchange) {
+			return 1;
+		}
+		if (this.stockchange < other.stockchange) {
+			return -1;
+		}
+		if (this.stockname.compareTo(other.stockname) > 0) {
+			return 1;
+		}
+		if (this.stockname.compareTo(other.stockname) < 0) {
+			return -1;
+		}
+		return 0;
+	}
+	public String toString() {
+		String str = "";
+		str = str + this.stockname + this.stockchange;
+		return str;
+	}
+}
+class Solution {
+	private Solution() {
+		//unused.
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int stocknum = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < 6; i++) {
+			int count = 0;
+			MinPQ<Stock> minpqobj = new MinPQ<>();
+			MaxPQ<Stock> maxpqobj = new MaxPQ<>();
+			while (count < stocknum) {
+				String[] details = scan.nextLine().split(",");
+				Stock stockobj = new Stock(details[0], Float.parseFloat(details[1]));
+				minpqobj.insert(stockobj);
+				maxpqobj.insert(stockobj);
+				count++;
+			}
+			for(Stock eachdetail : minpqobj) {
+				System.out.println(eachdetail);
+			}
+			for(Stock eachdetail2 : maxpqobj) {
+				System.out.println(eachdetail2);
+			}
+			// BinarySearchTree<String, FLoat> stockbest = new  BinarySearchTree<>();
+			// BinarySearchTree<String, FLoat> stockworst = new BinarySearchTree<>();
+			// for(int j = 0; j < 5; j++) {
+
+			// } 
+		}
+	}
+}
