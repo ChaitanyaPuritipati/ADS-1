93e07ed50333303b4fd135245fcbfae51c4c71a7
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-25 22:06:39 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/m22/22.1 Ransom Note/Solution.java
+++ ADS-1 Assignments/m22/22.1 Ransom Note/Solution.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
-class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		//unused
 	}
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] sizes = scan.nextLine().split(" ");
 		int magwordsnum = Integer.parseInt(sizes[0]);
 		int ransomwordsnum = Integer.parseInt(sizes[1]);
-		SeparateChainingHashST<String, Integer> schstobj = new SeparateChainingHashST<>();
+		SeparateChainingHashST<String, Integer> schstobj
+		    = new SeparateChainingHashST<>();
 		String[] magwords = scan.nextLine().split(" ");
 		String[] ransomwords = scan.nextLine().split(" ");
 		for (int i = 0; i < magwordsnum; i++) {
@@ -19,21 +31,16 @@ class Solution {
 				schstobj.put(magwords[i], 1);
 			}
 		}
-		int flag = 0;
 		for (int j = 0; j < ransomwordsnum; j++) {
-			if (schstobj.contains(ransomwords[j]) && schstobj.get(ransomwords[j]) > 0) {
+			if (schstobj.contains(ransomwords[j])
+			        && schstobj.get(ransomwords[j]) > 0) {
 				int count = schstobj.get(ransomwords[j]);
 				schstobj.put(ransomwords[j], --count);
 			} else {
-				// flag = 1;
 				System.out.println("No");
 				return;
 			}
 		}
-		// if (flag == 1) {
-		// 	System.out.println("No");
-		// } else {
-			System.out.println("Yes");
-		// }
+		System.out.println("Yes");
 	}
 }
