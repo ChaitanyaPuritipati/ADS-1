71712268c6866be6511df33d022ff9598414efda
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-27 13:08:59 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 Assignments/Module 24/m24/1/Solution.java
+++ ADS-1 Assignments/Module 24/m24/1/Solution.java
@@ -3,95 +3,95 @@ import java.util.Scanner;
  * Class for student.
  */
 class Student {
-	/**
-	 * { studentname }.
-	 */
-	private String studentname;
-	/**
-	 * { totalmarks }.
-	 */
-	private double totalmarks;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      name   The name
-	 * @param      marks  The marks
-	 */
-	Student(String name, double marks) {
-		this.studentname = name;
-		this.totalmarks = marks;
-	}
-	/**
-	 * { getname function }.
-	 * Complexity is O(1).
-	 *
-	 * @return     { String type }
-	 */
-	public String getname() {
-		return this.studentname;
-	}
-	/**
-	 * { getmarks function }.
-	 * Complexity is O(1).
-	 * @return     { description_of_the_return_value }
-	 */
-	public double getmarks() {
-		return this.totalmarks;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		String str = "";
-		str = str + this.studentname + this.totalmarks;
-		return str;
-	}
+    /**
+     * { studentname }.
+     */
+    private String studentname;
+    /**
+     * { totalmarks }.
+     */
+    private double totalmarks;
+    /**
+     * Constructs the object.
+     *
+     * @param      name   The name
+     * @param      marks  The marks
+     */
+    Student(String name, double marks) {
+        this.studentname = name;
+        this.totalmarks = marks;
+    }
+    /**
+     * { getname function }.
+     * Complexity is O(1).
+     *
+     * @return     { String type }
+     */
+    public String getname() {
+        return this.studentname;
+    }
+    /**
+     * { getmarks function }.
+     * Complexity is O(1).
+     * @return     { description_of_the_return_value }
+     */
+    public double getmarks() {
+        return this.totalmarks;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        str = str + this.studentname + this.totalmarks;
+        return str;
+    }
 }
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * { main function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int putnum = Integer.parseInt(scan.nextLine());
-		SeparateChainingHashST<Integer, Student> schsobj = new SeparateChainingHashST<>();
-		for (int i = 0; i < putnum; i++) {
-			String[] tokens = scan.nextLine().split(",");
-			Student studentobj = new Student(tokens[1], Double.parseDouble(tokens[2]));
-			schsobj.put(Integer.parseInt(tokens[0]), studentobj);
-		}
-		int querynum = Integer.parseInt(scan.nextLine());
-		for (int j = 0; j < querynum; j++) {
-			String[] querytokens = scan.nextLine().split(" ");
-			if (querytokens[0].equals("get")) {
-				if (Integer.parseInt(querytokens[2]) == 1) {
-					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
-						System.out.println("Student doesn't exists...");
-					} else {
-						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getname());
-					}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int putnum = Integer.parseInt(scan.nextLine());
+        SeparateChainingHashST<Integer, Student> schsobj = new SeparateChainingHashST<>();
+        for (int i = 0; i < putnum; i++) {
+            String[] tokens = scan.nextLine().split(",");
+            Student studentobj = new Student(tokens[1], Double.parseDouble(tokens[2]));
+            schsobj.put(Integer.parseInt(tokens[0]), studentobj);
+        }
+        int querynum = Integer.parseInt(scan.nextLine());
+        for (int j = 0; j < querynum; j++) {
+            String[] querytokens = scan.nextLine().split(" ");
+            if (querytokens[0].equals("get")) {
+                if (Integer.parseInt(querytokens[2]) == 1) {
+                    if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
+                        System.out.println("Student doesn't exists...");
+                    } else {
+                        System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getname());
+                    }
 
-				} else {
-					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
-						System.out.println("Student doesn't exists...");
-					} else {
-						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getmarks());
-					}
-				}
-			}
-		}
-	}
+                } else {
+                    if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
+                        System.out.println("Student doesn't exists...");
+                    } else {
+                        System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getmarks());
+                    }
+                }
+            }
+        }
+    }
 }
