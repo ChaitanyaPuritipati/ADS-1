1a22e4f453e02121a18bb789db0fb2f8f018c8fa
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-10-27 11:21:27 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 Assignments/Module 24/m24/2/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Student {
+	private String studentname;
+	private Integer rollnumber;
+	Student(String name, Integer  num) {
+		this.studentname = name;
+		this.rollnumber = num;
+	}
+	public String getname() {
+		return this.studentname;
+	}
+	public Integer getnumber() {
+		return this.rollnumber;
+	}
+	public String toString() {
+		String str = "";
+		str = str + this.studentname + this.rollnumber;
+		return str;
+	}
+}
+class Solution {
+	private Solution() {
+		//unused.
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int putnum = Integer.parseInt(scan.nextLine());
+		RedBlackBST<Double, Student> redobj = new RedBlackBST<>();
+		for(int i = 0; i < putnum; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			Student studentobj = new Student(tokens[1], Integer.parseInt(tokens[0]));
+			redobj.put(Double.parseDouble(tokens[2]), studentobj);
+		}
+	}
+}
